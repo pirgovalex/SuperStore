4f2e11e17476249ab87a4e66c1d759a61e8c80c0
Aleksandar Pirgov <stu2301261049@uni-plovdiv.bg>
2024-11-30 23:45:26 +0200

    first commit

---
+++ src/IOHandler/InputFileData.java
@@ -0,0 +1,43 @@
+package IOHandler;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+  class InputFileData {
+    private Map<String, List<String>> subclassMap = new HashMap<>();
+    private  String currentSubClass = null;
+
+    protected void dataHandler() throws IOException {
+        Path pathToFile =  Path.of("categories_instances.txt");
+        System.out.println(pathToFile);
+        List<String> lines = Files.readAllLines(pathToFile);
+
+
+
+        for (String line : lines){
+            line = line.trim();
+            if (line.startsWith("<")&&line.endsWith(">")&&!line.startsWith("</")){
+                currentSubClass = line.substring(1,line.length()-1);
+                subclassMap.putIfAbsent(currentSubClass, new ArrayList<>());
+
+            }
+            else if(currentSubClass!=null&& line.startsWith("*")&&!line.startsWith("</")){
+
+                line= line.replaceFirst("\\*", "");
+                String[] result =  line.split(",");
+
+                subclassMap.get(currentSubClass).add(line);
+
+            }
+        }
+
+        subclassMap.forEach((subclass,instances)-> {
+            System.out.println("Subclass: " + subclass);
+            System.out.println("Instances: " + instances);
+        });
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import IOHandler.InputFileData;
+
+import java.io.IOException;
+
+public class Main {
+
+ public static  void main(String[] args) throws IOException {
+     InputFileData IFD = new InputFileData();
+     IFD.dataHandler();
+    }
+}
